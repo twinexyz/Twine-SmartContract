ef17c8256df6351610e39ee6127acd7d717f8162
Sarthak-5harma <sarthak.sharma@cedro.finance>
2024-10-04 15:08:52 +0545

    verifier contract added

--- src/L1/rollup/ITwineChain.sol
+++ src/L1/rollup/ITwineChain.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
+
 interface ITwineChain {
     /// @notice Emitted when a new batch is committed.
     /// @param batchIndex The index of the batch.
     /// @param batchHash The hash of the batch.
     event CommitBatch(uint256 indexed batchIndex, bytes32 indexed batchHash);
+
+    // @notice revert a pending batch.
+    /// @param batchIndex The index of the batch.
+    /// @param batchHash The hash of the batch
+    event RevertBatch(uint256 indexed batchIndex, bytes32 indexed batchHash);
 
     /// @notice Emitted when a batch is finalized.
     /// @param batchIndex The index of the batch.
@@ -35,10 +42,12 @@ interface ITwineChain {
 
     /// @notice Commit a batch of transactions on layer 1.
     ///
+    /// @param version The version of current batch
     /// @param parentBatchHeader The header of parent batch, see the comments of `BatchHeaderV0Codec`.
     /// @param chunks The list of encoded chunks, see the comments of `ChunkCodec`.
     /// @param skippedL1MessageBitmap The bitmap indicates whether each L1 message is skipped or not.
     function commitBatch(
+        uint8 version,
         bytes calldata parentBatchHeader,
         bytes[] memory chunks,
         bytes calldata skippedL1MessageBitmap
