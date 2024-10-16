99270a63ceb74673cf79029dcc85cec26b547e38
Sarthak-5harma <sarthak.sharma@cedro.finance>
2024-10-16 15:15:16 +0545

    Basic Commitment Flow

--- src/L1/rollup/TwineChain.sol
+++ src/L1/rollup/TwineChain.sol
@@ -10,29 +10,10 @@ import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 /// @title TwineChain
 /// @notice This contract maintains the data for Meta Rollup.
 contract TwineChain is ITwineChain {
-    /**********
-     * Errors *
-     **********/
 
+    /// @dev Thrown when the given address is `address(0)`.
     error ErrorZeroAddress();
-
-    /// @dev Thrown when the given account is not EOA account.
-    error ErrorAccountIsNotEOA();
-
-    /// @dev Thrown when committing a committed batch.
-    error ErrorBatchIsAlreadyCommitted();
-
-    /// @dev Thrown when finalizing a verified batch.
-    error ErrorBatchIsAlreadyVerified();
-
-    /// @dev Thrown when committing empty batch (batch without chunks)
-    error ErrorBatchIsEmpty();
-
-    uint256 internal constant BATCH_HEADER_FIXED_LENGTH = 89;
-
-    /// @notice The chain id of the corresponding layer 2 chain.
-    uint64 public immutable layer2ChainId;
-
+    
     /// @notice The address of L1MessageQueue contract.
     address public immutable messageQueue;
 
@@ -40,204 +21,79 @@ contract TwineChain is ITwineChain {
     address public immutable verifier;
 
     /// @inheritdoc ITwineChain
-    uint256 public override lastFinalizedBatchIndex;
-
-    /// @inheritdoc ITwineChain
     mapping(uint256 => bytes32) public override committedBatches;
 
     /// @inheritdoc ITwineChain
     mapping(uint256 => bytes32) public override finalizedStateRoots;
 
-    /// @inheritdoc ITwineChain
-    mapping(uint256 => bytes32) public override withdrawRoots;
+    struct StoredBatchInfo {
+        uint64 batchNumber;
+        bytes32[] transactionList;
+        bytes32 stateRoot;
+        bytes32 transactionRoot;
+        bytes32 publicInput;
+    }
 
+    mapping (uint256 => StoredBatchInfo) public StoredBatch;
+
+    struct CommitBatchInfo {
+        uint64 batchNumber;
+        bytes32[] transactionList;
+        bytes32 newStateRoot;
+        bytes32 transactionRoot;
+    }
+
+    StoredBatchInfo public lastBatchData;
     /***************
      * Constructor *
      ***************/
 
     /// @notice Constructor for `ScrollChain` implementation contract.
     ///
-    /// @param _chainId The chain id of L2.
     /// @param _messageQueue The address of `L1MessageQueue` contract.
     /// @param _verifier The address of zkevm verifier contract.
 
 
     constructor(
-        uint64 _chainId,
         address _messageQueue,
         address _verifier
     ) {
         if (_messageQueue == address(0) || verifier == address(0)) {
             revert ErrorZeroAddress();
         }
-        layer2ChainId = _chainId;
         messageQueue = _messageQueue;
         verifier = _verifier;
     }
 
-    /*************************
-     * Public View Functions *
-     *************************/
-
-    /// @inheritdoc ITwineChain
-    function isBatchFinalized(uint256 _batchIndex) external view override returns (bool) {
-        return _batchIndex <= lastFinalizedBatchIndex;
+    function commitBatch(CommitBatchInfo calldata _newBatchData) external {
+        lastBatchData = _commitBatch(_newBatchData);
+        StoredBatch[lastBatchData.batchNumber] = lastBatchData;
     }
 
-    /*****************************
-     * Public Mutating Functions *
-     *****************************/
-
-     /// @inheritdoc ITwineChain
-     function commitBatch(
-        bytes calldata _parentBatchHeader, 
-        bytes[] memory _chunks,
-        bytes calldata _skippedL1MessageBitmap
-    ) external override {
-        (bytes32 _parentBatchHash, uint256 _batchIndex, uint256 _totalL1MessagePoppedOverall) = _beforeCommitBatch(
-            _parentBatchHeader, 
-            _chunks
-        );
-
-        bytes32 _batchHash;
-        uint256 batchPtr;
-        bytes32 _dataHash;
-        uint256 _totalL1MessagePoppedInBatch;
-        (_dataHash, _totalL1MessagePoppedInBatch) = _commitChunks(
-            _totalL1MessagePoppedOverall,
-            _chunks,
-            _skippedL1MessageBitmap
-        );
-        assembly {
-            batchPtr := mload(0x40)
-            _totalL1MessagePoppedOverall := add(_totalL1MessagePoppedOverall, _totalL1MessagePoppedInBatch)
-        }
-
-        storeBatchIndex(batchPtr, _batchIndex);
-        storeL1MessagePopped(batchPtr, _totalL1MessagePoppedInBatch);
-        storeTotalL1MessagePopped(batchPtr, _totalL1MessagePoppedOverall);
-        storeDataHash(batchPtr, _dataHash);
-        storeParentBatchHash(batchPtr, _parentBatchHash);
-        storeSkippedBitmap(batchPtr, _skippedL1MessageBitmap);
-
-        // compute batch hash
-        _batchHash = computeBatchHash(batchPtr, BATCH_HEADER_FIXED_LENGTH + _skippedL1MessageBitmap.length);
-
-        _afterCommitBatch(_batchIndex, _batchHash);
+    function _commitBatch(CommitBatchInfo calldata _newBatchData) internal returns (StoredBatchInfo memory) {
+        require(_newBatchData.batchNumber != lastBatchData.batchNumber + 1, "Only next batch can be committed.");
+        bytes32 proofInput = _calculateProofInput(_newBatchData);
+        return
+            StoredBatchInfo({
+                batchNumber: _newBatchData.batchNumber,
+                transactionList: _newBatchData.transactionList,
+                stateRoot: _newBatchData.stateRoot,
+                transactionRoot: _newBatchData.transactionRoot,
+                publicInput: proofInput
+            });
     }
 
-    function _beforeCommitBatch(bytes calldata _parentBatchHeader, bytes[] memory _chunks)
-        private
-        view
-        returns (
-            bytes32 _parentBatchHash,
-            uint256 _batchIndex,
-            uint256 _totalL1MessagesPoppedOverall
-        )
-    {
-        // checks whether the batch is empty
-        if (_chunks.length == 0) revert ErrorBatchIsEmpty();
-        (, _parentBatchHash, _batchIndex, _totalL1MessagesPoppedOverall) = _loadBatchHeader(_parentBatchHeader);
-        unchecked {
-            _batchIndex += 1;
-        }
-        if (committedBatches[_batchIndex] != 0) revert ErrorBatchIsAlreadyCommitted();
-    }
+    function _calculateProofInput(CommitBatchInfo calldata _newBatchData) internal returns (bytes32) {
 
-    function _afterCommitBatch(uint256 _batchIndex,bytes32 _batchHash) private {
-        committedBatches[_batchIndex] = _batchHash;
-        emit CommitBatch(_batchIndex, _batchHash);
-    } 
+        // First we have to seperate out (L1 transaction object) and (L2 trasnactions + L1 transaction object of other L1s)
+        bytes32 L1transaction = _extractL1Transaction(_newBatchData.transactionList);
+        bytes32[] L2transactions = _extractL2Transaction(_newBatchData.transactionList);
 
+        // We need to calculate L1TransactionObject just like how relayer is calculating.
+        bytes32 L1transactionObject = _calculateTransactionObject(L1transactions);
 
-    function _loadBatchHeader(bytes calldata _batchHeader)
-        internal
-        view
-        virtual
-        returns (
-            uint256 batchPtr,
-            bytes32 _batchHash,
-            uint256 _batchIndex,
-            uint256 _totalL1MessagePoppedOverall
-        )
-    {
-        uint256 _length;
-        (batchPtr, _length) = _loadAndValidate(_batchHeader);
-
-        _batchHash = computeBatchHash(batchPtr, _length);
-        _batchIndex = getBatchIndex(batchPtr);
-        _totalL1MessagePoppedOverall = getTotalL1MessagePopped(batchPtr);
-    }
-     
-    function _loadAndValidate(bytes calldata _batchHeader) internal pure returns (uint256 batchPtr, uint256 length) {
-        length = _batchHeader.length;
-        
-        //copy batch header to memory.
-        assembly {
-            batchPtr := mload(0x40)
-            calldatacopy(batchPtr, _batchHeader.offset, length)
-            mstore(0x40, add(batchPtr, length))
-        }
-    }
-
-    function computeBatchHash(uint256 batchPtr, uint256 length) internal pure returns (bytes32 _batchHash) {
-        assembly {
-            _batchHash := keccak256(batchPtr, length)
-        }
-    }
-
-    function getBatchIndex(uint256 batchPtr) internal pure returns (uint256 _batchIndex) {
-        assembly {
-            _batchIndex := shr(192, mload(add(batchPtr, 0)))
-        }
-    }
-
-    function getTotalL1MessagePopped(uint256 batchPtr) internal pure returns (uint256 _totalL1MessagePopped) {
-        assembly {
-            _totalL1MessagePopped := shr(192, mload(add(batchPtr, 17)))
-        }
-    }
-
-
-    ////// STORING INDIVIDUALM BATH INFORMATION IN MEMORY
-
-    function storeBatchIndex(uint256 batchPtr, uint256 _batchIndex) internal pure {
-        assembly {
-            mstore(add(batchPtr, 1), shl(192, _batchIndex))
-        }
-    }
-
-    function storeL1MessagePopped(uint256 batchPtr, uint256 _totalL1MessagePoppedInBatch) internal pure {
-        assembly {
-            mstore(add(batchPtr, 9), shl(192, _totalL1MessagePoppedInBatch))
-        }
-    }
-    function storeTotalL1MessagePopped(uint256 batchPtr, uint256 _totalL1MessagePoppedOverall)  internal pure {
-        assembly {
-            mstore(add(batchPtr, 17), shl(192, _totalL1MessagePoppedOverall))
-        }
-    }
-    function storeDataHash(uint256 batchPtr, bytes32 _dataHash)  internal pure {
-        assembly {
-            mstore(add(batchPtr, 25), shl(192, _dataHash))
-        }
-    }
-    function storeParentBatchHash(uint256 batchPtr, bytes32 _parentBatchHash)  internal pure {
-        assembly {
-            mstore(add(batchPtr, 57), shl(192, _parentBatchHash))
-        }
-    }
-    function storeSkippedBitmap(uint256 batchPtr, bytes calldata _skippedL1MessageBitmap) internal pure {
-        assembly {
-            calldataCopy(
-                add(batchPtr, BATCH_HEADER_FIXED_LENGTH),
-                _skippedL1MessageBitmap.offset,
-                _skippedL1MessageBitmap.length
-            )
-        }
-    }
-
-
-
+        // We will append (stateroot + l1txnobject + l2txns) and that is our public input
+        bytes32 inputs = _calculateProof(_newBatchData.newStateRoot, L1transactions, L2transactions);
+        return inputs;
+    }   
 }
-
