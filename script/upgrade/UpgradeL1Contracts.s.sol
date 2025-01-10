4d64c5d70cc350547debc6194b971f00bd2ba50c
lilixac <hinewton25@gmail.com>
2025-01-10 09:34:50 +0545

    update script

--- script/upgrade/UpgradeL1Contracts.s.sol
+++ script/upgrade/UpgradeL1Contracts.s.sol
@@ -98,12 +98,12 @@ contract UpgradeL1Contracts is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        TwineChain newTwineChain = new TwineChain();
-        getProxyAdmin(twineChainAddress).upgradeAndCall(
-            ITransparentUpgradeableProxy(twineChainAddress),
-            address(newTwineChain),
-            data
-        );
+        // TwineChain newTwineChain = new TwineChain();
+        // getProxyAdmin(twineChainAddress).upgradeAndCall(
+        //     ITransparentUpgradeableProxy(twineChainAddress),
+        //     address(newTwineChain),
+        //     data
+        // );
 
         // L1ETHGateway newL2ETHGateway = new L1ETHGateway();
         // getProxyAdmin(l1ETHGatewayAddress).upgradeAndCall(
@@ -126,12 +126,12 @@ contract UpgradeL1Contracts is Script {
         //     data
         // );
 
-        // L1TwineMessenger newL1TwineMessenger = new L1TwineMessenger();
-        // getProxyAdmin(l1TwineMessengerAddress).upgradeAndCall(
-        //     ITransparentUpgradeableProxy(l1TwineMessengerAddress),
-        //     address(newL1TwineMessenger),
-        //     data
-        // );
+        L1TwineMessenger newL1TwineMessenger = new L1TwineMessenger();
+        getProxyAdmin(l1TwineMessengerAddress).upgradeAndCall(
+            ITransparentUpgradeableProxy(l1TwineMessengerAddress),
+            address(newL1TwineMessenger),
+            data
+        );
 
         // L1CustomERC20Gateway newL1CustomERC20Gateway = new L1CustomERC20Gateway();
         // getProxyAdmin(l1CustomERC20GatewayAddress).upgradeAndCall(
